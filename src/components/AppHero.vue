b2616695a1413b299f7df4bf6a5e50430fa85831
donatomontrone <donato.montrone@outlook.com>
2022-12-23 16:01:43 +0100

    added title and jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<template>
+    <div class="jumbotron">
+    </div>
+</template>
+
+<style>
+div.jumbotron {
+    background-image: url(../assets/apple-bg.jpg);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 90vh;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<template>
+    <h1> La mia prima app con Vite! </h1>
+</template>
+
+<style>
+h1 {
+    text-align: center;
+}
+</style>
